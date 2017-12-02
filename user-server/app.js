8528421c2721fe40ae620d1955adada8f40bea3a
Pelumi Odimayo <podimayo@gmail.com>
2017-12-02 17:50:51 -0500

    Starting api

--- user-server/app.js
+++ user-server/app.js
@@ -1,29 +1,43 @@
+/*
+ * app.js
+ *
+ * Sets up the server and API route.
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Import route for API
+var index = require('./routes/api');
 
 var app = express();
 
+// Link API route to app
+app.use('/api', api);
+
+// Connect to database
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "password"
+});
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +58,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+module.exports = con;
