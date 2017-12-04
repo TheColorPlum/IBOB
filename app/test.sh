29a122aff9262ca738986e39e04488681d23d97d
Michael J Friedman <mfred240@gmail.com>
2017-12-03 19:29:02 -0500

    Set up Mocha testing framework for user-server
    - Updated documentation to explain rules for writing/running
      tests.
    - You will need to rerun `npm install` to add new dependencies
      for testing.

---
+++ app/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Michael Friedman
+#
+# Shortcut for running all tests for this component.
+
+npm test
---
+++ user-server/test/aps.js
@@ -0,0 +1,11 @@
+/*
+ * aps.js
+ * Authors: Michael Friedman and Pelumi Odimayo
+ *
+ * Tests for the authentication/permission system.
+ */
+
+var assert = require("assert");
+var aps = require("../routes/aps");
+
+// TODO: Write tests here.
