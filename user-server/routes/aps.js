3a45b3be60deb61a840ffd01f11c3a481a9cd783
Michael J Friedman <mfred240@gmail.com>
2017-12-03 22:58:52 -0500

    Implemented APS verifyRequest() and tests (untested)
    - verifyRequest() is written according to design doc spec,
      except it's missing a way of getting the owner of this
      user-server. See isOwner() in aps.js.
    - Tests are implemented but cannot be run yet because I still
      need to get/find the private keys of the test users.

--- user-server/routes/aps.js
+++ user-server/routes/aps.js
@@ -6,25 +6,101 @@
  * (see documentation for details).
  */
 
+import { TokenVerifier, decodeToken } from 'jsontokens'
+const axios = require("axios");
+
 // Permission levels
-permissions = {
+const permissions = {
     owner: 0,
     regular: 1
 };
 
+// JWT encoding algorithm
+const encAlg = "ES256k";
+
+// Blockstack core API url
+const blockstackBaseUrl = "http://localhost:6270";
+const blockstackProfileExt = "/v1/names/";
+
+// Regex for Blockstack zonefiles
+const zonefileRegex = "https://gaia.blockstack.org/hub/[A-Za-z0-9]+/[0-9]+/profile.json";
+
+/*
+ * Helper function to test whether a user is the owner. Returns boolean
+ * indicator.
+ */
+function isOwner(user) {
+    // TODO: Implement isOwner() (need to get owner from the database and compare)
+    return user === "alice.id";
+}
 
 /*
  * Authenticates the requester (requester, their Blockstack ID) as the one who
  * actually sent the request (encData, a JWT) and has the permission level
  * specified (reqPermission, one of the above permissions).
  *
+ * Returns a Promise for the object:
+ *   {ok (boolean), decodedData (string), errorMsg (string)}
+ * where if ok is true, decodedData contains the data; and if
+ * ok is false, errorMsg contains a message and there is no
+ * decodedData.
+ *
  * See documentation for more details.
  */
 function verifyRequest(encData, requester, reqPermission) {
-    // TODO: Implement verifyRequest()
-    return {
-        ok: false,
-        decodedData: "",
-        errorMsg: "Request denied: authentication failed."
-    }
+    //-----------------------------
+    // Step 1: Authenticate user
+    //-----------------------------
+
+    // Get user's public key from blockstack. First, get
+    // their profile.
+    var requesterUrl = blockstackBaseUrl + blockstackProfileExt + requester;
+    return axios.get(requesterUrl).then(response => {
+
+        var json = response.data;
+
+        // Make sure user is registered
+        if (json.status !== "registered") {
+            return {ok: false, decodedData: "", errorMsg: "Denied: User is not registered"};
+        }
+
+        // Get user's zone file
+        var zonefileUrl = json.zonefile.match(zonefileRegex)[0];
+        return axios.get(zonefileUrl).then(response => {
+
+            var json = response.data;
+            var publicKey = json[0].payload.subject.publicKey;
+
+            // Check signature on request
+            var verified = new TokenVerifier(encAlg, publicKey).verify(encData);
+            if (!verified) {
+                return {ok: false, decodedData: "", errorMsg: "Denied: Signature is invalid"};
+            }
+
+            //-----------------------------
+            // Step 2: Check permissions
+            //-----------------------------
+
+            // Make sure a non-owner is not trying to access owner permissions
+            if (!isOwner(requester) && reqPermission === permissions.owner) {
+                return {ok: false, decodedData: "", errorMsg: "Denied: User does not have sufficient permissions"};
+            }
+
+            //-------------------------------------------------
+            // Step 3: Requester is authenticated and granted
+            // permission. Decode the token and return
+            //-------------------------------------------------
+
+            decodedData = decodeToken(encData);
+            return {ok: true, decodedData: decodedData, errorMsg: ""}; // success!
+
+
+        }).catch(error => { // failed to get requester's zonefile
+            return {ok: false, decodedData: "", errorMsg: "Error: Request to Blockstack for " + requester + "'s zonefile failed."};
+        });
+
+    }).catch(error => { // failed to get requester's profile
+        return {ok: false, decodedData: "", errorMsg: "Error: Request to Blockstack for " + requester + "'s profile failed."};
+    });
+
 }
--- user-server/test/aps.js
+++ user-server/test/aps.js
@@ -5,7 +5,76 @@
  * Tests for the authentication/permission system.
  */
 
+import { TokenSigner } from 'jsontokens';
+
 var assert = require("assert");
 var aps = require("../routes/aps");
 
-// TODO: Write tests here.
+// Tests here
+
+describe("Authenticating and granting permission to users", function() {
+
+    var alice   = "alice.id";
+    var bob     = "bob.id";
+    var mallory = "mallory.id";
+
+    // TODO: Get private keys from Blockstack browser
+    var alicePrivateKey   = "";
+    var bobPrivateKey     = "";
+    var malloryPrivateKey = "";
+
+    var data = {id: 0, content: "This is some sample text"};
+    var encAlg = "ES256k";
+    var aliceEncData   = new TokenSigner(encAlg, alicePrivateKey).sign(data);
+    var bobEncData     = new TokenSigner(encAlg, bobPrivateKey).sign(data);
+    var malloryEncData = new TokenSigner(encAlg, malloryPrivateKey).sign(data);
+
+    // TODO: Before tests, set up database with Alice as owner.
+
+
+    // Test normal use
+    it("Verifies owner requesting owner-level permissions", function() {
+        verification = aps.verifyRequest(aliceEncData, alice, aps.permissions.owner);
+
+        assert.true(verification.ok);
+        assert.equal(verification.decodedData, JSON.stringify(data));
+        assert.equal(verification.errorMsg, "");
+    });
+
+    it("Verifies owner requesting regular permissions", function() {
+        verification = aps.verifyRequest(aliceEncData, alice, aps.permissions.regular);
+
+        assert.true(verification.ok);
+        assert.equal(verification.decodedData, JSON.stringify(data));
+        assert.equal(verification.errorMsg, "");
+    });
+
+    it("Verifies regular user requesting regular permissions", function() {
+        verification = aps.verifyRequest(bobEncData, bob, aps.permissions.regular);
+
+        assert.true(verification.ok);
+        assert.equal(verification.decodedData, JSON.stringify(data));
+        assert.equal(verification.errorMsg, "");
+    });
+
+
+    // Test user pretending to be someone else
+    it("Denies a user with an invalid signature", function() {
+        // Mallory pretending to be Alice
+        verification = aps.verifyRequest(malloryEncData, alice, aps.permissions.regular);
+
+        assert.false(verification.ok);
+        assert.equal(verification.decodedData, "");
+        assert.notEqual(verification.errorMsg, "");
+    });
+
+    // Test non-owner trying to access owner permissions
+    it("Denies a regular user requesting owner-level permissions", function() {
+        // Mallory tries to get owner-level permissions
+        verification = aps.verifyRequest(malloryEncData, mallory, aps.permissions.owner);
+
+        assert.false(verification.ok);
+        assert.equal(verification.decodedData, "");
+        assert.notEqual(verification.errorMsg, "");
+    });
+});
