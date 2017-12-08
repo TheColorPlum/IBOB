162c05d0109c079e0515c0d04adeffa9c159b35a
Michael J Friedman <mfred240@gmail.com>
2017-12-07 21:15:23 -0500

    Implemented GET /profile-info request
    - Added a dummy version of data access layer for the sake
      of testing this GET request.
    - Wrote tests, which work without doing the APS verification
      step, but don't work with it (since APS is not fully working
      yet).

--- user-server/test.sh
+++ user-server/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Author: Michael Friedman
 #
-# Shortcut for running all tests for the user-server.
+# Shortcut for running all tests for this component.
 
+echo "Warning: Make sure you are running the server"
+sleep 2
+
+# Run tests
 npm test
--- user-server/test/api.js
+++ user-server/test/api.js
@@ -7,5 +7,65 @@
 
 var assert = require("assert");
 var api = require("../routes/api");
+var axios = require("axios");
+var dal = require("../dal");
 
-// TODO: Write tests here.
+const baseUrl = "http://localhost:3000";
+
+// Tests here
+
+describe("GET /api" + api.urls.getProfileInfo, function() {
+
+    it("Returns correct profile info", function() {
+        // Define profile info
+        var profileInfo = {
+            id: "alice.id",
+            name: "Alice",
+            bio: "Sample bio",
+            profilePhoto: 11,
+            coverPhoto: 21
+        };
+
+        var following = ["bob.id", "mallory.id"];
+
+        // Insert profile info into the database
+        dal.updateProfileInfo(profileInfo);
+        following.forEach(user => {
+            dal.addFollowing(user);
+        });
+
+        // Make request
+        var reqBody = {"timestamp": (new Date()).toJSON()};
+        return axios.post(baseUrl + "/api" + api.urls.getProfileInfo + "?requester=alice.id", reqBody)
+        .then(resp => {
+            // Check that response has all attributes
+            var json = resp.data;
+            for (attr in profileInfo) {
+                assert(json.hasOwnProperty(attr), "Response is missing attribute: " + attr);
+                assert.strictEqual(json[attr], profileInfo[attr], "Response has wrong value for attribute: " + attr);
+            }
+            assert(json.hasOwnProperty("following"), "Response is missing attribute: following");
+
+            // Check that response has only those attributes
+            var numAttrs = Object.keys(json).length;
+            var correctNumAttrs = Object.keys(profileInfo).length + 1;
+            assert.strictEqual(numAttrs, correctNumAttrs, "Response has more attributes than it should. Response:\n" + JSON.stringify(json));
+
+            // Check that "following" lists match
+            assert.deepStrictEqual(json.following, following, "Response's 'following' list is incorrect. Response: " + json.following + "; actual: " + following);
+        });
+    });
+});
+
+
+describe("GET /api" + api.urls.getPosts, function() {
+
+    it("Returns correct posts", function() {
+
+    });
+});
+
+
+describe("GET /api" + api.urls.getFeed, function() {
+
+});
