61e174bccd47876a2b7e24752699ef381846051f
Michael J Friedman <mfred240@gmail.com>
2017-12-24 10:53:56 -0500

    Initial framework for the user-server directory
    - Created package.json file and installed dependencies
    - Wrote server.js, which runs the server (though it doesn't do
      anything yet)

---
+++ directory/routes/api.js
@@ -0,0 +1,43 @@
+/*
+ * api.js
+ * Authors: Michael Friedman and Pelumi Odimayo
+ *
+ * Implements the user-server's web API (see documentation for details).
+ */
+
+var express = require("express");
+var app = express();
+
+/******************************************************************************/
+
+const urls = {
+    get: "/get",
+    put: "/put"
+}
+
+
+/*
+ * Returns the IP address of a user's user-server.
+ */
+app.post(urls.get, function(req, res, next) {
+
+    // TODO: Implement
+    res.json({success: false, msg: "Not implemented"});
+});
+
+
+
+/*
+ * Adds a (bsid, user-server IP address) mapping to the directory.
+ */
+app.post(urls.put, function(req, res, next) {
+
+    // TODO: Implement
+    res.json({success: false, msg: "Not implemented"});
+});
+
+
+module.exports = {
+    app,
+    urls
+};
