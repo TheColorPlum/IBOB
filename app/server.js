fa47d056b8bd2982ecc43c3b0a8cdd9cf654a5ce
Michael J Friedman <mfred240@gmail.com>
2018-01-01 01:36:17 -0500

    Implemented app server
    - Simple server that just sends pages to the client. They will
      be populated with data on the client side.
    - Serves 4 pages: index (login), profile, feed, and error

--- app/server.js
+++ app/server.js
@@ -1,22 +1,102 @@
-const express = require('express')  
-const opn = require('opn')
+/*
+ * server.js
+ *
+ * Configures and starts the server.
+ */
 
-const app = express()  
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-function allowCrossDomain(req, res, next) {
+const axios = require('axios');
+const path = require('path');
+
+/******************************************************************************/
+
+// Configuration
+
+var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 }
 
-app.use(allowCrossDomain)
-app.use('/', express.static(__dirname + '/public'))
-app.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-  console.log(`server is listening on ${port}`)
-  opn('http://localhost:5000')
-})
+app.use(allowCrossDomain);
+
+// Expose static files
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+/******************************************************************************/
+
+// Serve pages
+
+// Url endpoints
+const urls = {
+    index: '/',
+    profile: '/profile/:bsid',
+    feed: '/feed',
+    error: '/error'
+};
+
+// Base directory of views
+const viewsDir = path.join(__dirname, '/views');
+
+// User-server directory url
+const directoryBaseUrl = 'http://localhost:4000';
+
+/*
+ * Returns the index (login) page
+ */
+app.get(urls.index, function(req, res, next) {
+    res.sendFile(path.join(viewsDir, 'index.html'));
+});
+
+
+/*
+ * Returns the profile page
+ */
+app.get(urls.profile, function(req, res, next) {
+    // Check that bsid requested has an account in the directory
+    var bsid = req.params.bsid;
+    var errorPage = path.join(viewsDir, '404.html');
+    axios.post(directoryBaseUrl + '/get')
+    .then(resp => {
+
+        var json = resp.data;
+        if (!json.success) {
+            res.redirect(urls.error);
+            return;
+        }
+
+        // Bsid exists, so we can send the profile page
+        res.sendFile(path.join(viewsDir, 'profile.html'));
+    })
+    .catch(err => {
+        res.redirect(urls.error);
+    });
+});
+
+
+/*
+ * Returns the feed page
+ */
+app.get(urls.feed, function(req, res, next) {
+    res.sendFile(path.join(viewsDir, 'feed.html'));
+});
+
+
+/*
+ * Returns a 404 error page
+ */
+app.get('/error', function(req, res, next) {
+    res.sendFile(path.join(viewsDir, '404.html'));
+});
+
+
+/******************************************************************************/
+
+// Start server
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
